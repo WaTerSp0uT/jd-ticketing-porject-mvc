05dae8755d304e26a907e4d2867ed8608dd8761d
WaTerSp0uT <ericyarek@gmail.com>
2020-12-07 00:08:28 -0500

    Add PostMaping for save Button

--- jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/UserController.java
+++ jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,14 +24,28 @@ public class UserController {
 
     @GetMapping({"/create","/add","/initialize"})
     public String createUser(Model model){
-        model.addAttribute("user", new UserDTO());
 
+
+        model.addAttribute("user", new UserDTO());
         //datagenerator
         //Since this class - UserController has dependecies from Role I need to inject dependencies
         model.addAttribute("roles", roleService.findAll());
-
         model.addAttribute("users", userService.findAll());
 
         return "/user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(UserDTO user, Model model){
+
+        userService.save(user);
+        //user, role users
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+
+
+        return "/user/create";
+
     }
 }
