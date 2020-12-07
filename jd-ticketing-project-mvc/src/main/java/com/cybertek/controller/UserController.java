736118813b181212bb4f0f9a84b3a73998bd26da
WaTerSp0uT <ericyarek@gmail.com>
2020-12-06 23:25:39 -0500

    Added Genrator data , FIxed bugs with html, binded list

--- jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/UserController.java
+++ jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.cybertek.controller;
 
-import com.cybertek.dto.RoleDTO;
+
 import com.cybertek.dto.UserDTO;
+import com.cybertek.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    //Injection
+    @Autowired
+    RoleService roleService;
+
     @GetMapping({"/create","/add","/initialize"})
     public String createUser(Model model){
         model.addAttribute("user", new UserDTO());
 
         //datagenerator
+        //Since this class - UserController has dependecies from Role I need to inject dependencies
+        model.addAttribute("roles", roleService.findAll());
 
         return "/user/create";
     }
