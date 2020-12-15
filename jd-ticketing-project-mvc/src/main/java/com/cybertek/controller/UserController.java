8f368261316e664e8311f73449fc316ac5846b89
WaTerSp0uT <ericyarek@gmail.com>
2020-12-14 21:12:57 -0500

    add userUpdate View

--- jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/UserController.java
+++ jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -48,4 +49,17 @@ public class UserController {
         return "/user/create";
 
     }
+
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+
+        //Service findByID will get specific data for particullar UserId and will assing to user for next population
+        model.addAttribute("user", userService.findById(username));
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("roles", roleService.findAll());
+
+        return "/user/update";
+    }
+
 }
