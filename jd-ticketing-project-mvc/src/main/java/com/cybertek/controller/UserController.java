6f2c92636f45be98f18789d56394c7cebad862b7
WaTerSp0uT <ericyarek@gmail.com>
2020-12-14 20:20:39 -0500

    Fix the covertor issue

--- jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/UserController.java
+++ jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
         return "/user/create";
     }
 
-    @PostMapping("/create")
+    @PostMapping("create")
     public String insertUser(UserDTO user, Model model){
 
         userService.save(user);
--- jd-ticketing-project-mvc/src/main/java/com/cybertek/converter/RoleDtoConverter.java
+++ jd-ticketing-project-mvc/src/main/java/com/cybertek/converter/RoleDtoConverter.java
@@ -3,12 +3,13 @@ package com.cybertek.converter;
 import com.cybertek.dto.RoleDTO;
 import com.cybertek.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RoleDtoConverter implements Converter<String, Object> {
+@ConfigurationPropertiesBinding
+public class RoleDtoConverter implements Converter<String, RoleDTO> {
 
     @Autowired
     RoleService roleService;
