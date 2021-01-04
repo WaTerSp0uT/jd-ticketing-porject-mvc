33c19a170fadf3bd3bfb401773122325ce69f43a
WaTerSp0uT <ericyarek@gmail.com>
2021-01-03 22:37:21 -0500

    Fixing project update, when satus was not catching

--- jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/ProjectController.java
+++ jd-ticketing-project-mvc/src/main/java/com/cybertek/controller/ProjectController.java
@@ -65,4 +65,12 @@ public class ProjectController {
         return "/project/update";
     }
 
+    @PostMapping("/update/{projectcode}")
+    public String updateProject(@PathVariable("projectcode") String projectcode, ProjectDTO project, Model model){
+
+        projectService.update(project);
+
+        return "redirect:/project/create";
+    }
+
 }
